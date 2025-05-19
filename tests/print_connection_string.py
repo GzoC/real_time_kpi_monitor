ea376b1eb3c03404178b9be39a5478ebf38a66dc
GzoC <cisternasalinasg@gmail.com>
2025-05-19 17:42:22 -0400

    Corregir la importación de SQLALCHEMY_DATABASE_URL para incluir el prefijo 'src.'

--- tests/print_connection_string.py
+++ tests/print_connection_string.py
@@ -4,7 +4,7 @@ import os
 # Agrega la carpeta 'src/' al path de importación
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from config import SQLALCHEMY_DATABASE_URL
+from src.config import SQLALCHEMY_DATABASE_URL
 
 print("Cadena de conexión cargada:")
 print(SQLALCHEMY_DATABASE_URL)
